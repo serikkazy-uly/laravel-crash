b637f421d2051b8084d295b30829b518b50e4ec3
serikkazy-uly <damirkap89@gmail.com>
2024-03-22 14:53:13 +0500

    feat: add routes and methods

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\NoteController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit',[NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
