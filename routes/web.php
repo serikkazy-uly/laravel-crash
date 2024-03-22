1880909feeccbecdb68ffb620887ede3ef17f334
serikkazy-uly <damirkap89@gmail.com>
2024-03-22 16:21:37 +0500

    feat: render all pages and Add welcome controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\WelcomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +15,7 @@ use App\Http\Controllers\NoteController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 
 // Route::get('/note', [NoteController::class, 'index'])->name('note.index');
 // Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
